826aea3a352e6e0e323078eceefb450fe587ccdb
Jesus Enríquez Jaime <jesus-e98@hotmail.com>
2026-06-20 18:34:03 -0700

    feat(chat): Enhance ChatInput with icon buttons and improve layout

--- client/app/chat.tsx
+++ client/app/chat.tsx
@@ -1,19 +1,25 @@
-import { View } from "react-native";
+import { KeyboardAvoidingView, Platform, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import ChatInput from "@/components/ChatInput";
 import MessageList from "@/components/MessageList";
+import { useHeaderHeight } from "@react-navigation/elements";
 
 export default function Chat() {
   const insets = useSafeAreaInsets();
+  const headerHeight = useHeaderHeight();
   return (
-    <View className="flex-1">
-      <View
-        className="flex-1 flex-col bg-blue-100"
-        style={{ paddingBottom: insets.bottom }}
+    <View
+      className="flex-1 bg-blue-100"
+      style={{ paddingBottom: insets.bottom }}
+    >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : undefined}
+        className="flex-1"
+        keyboardVerticalOffset={headerHeight}
       >
         <MessageList />
         <ChatInput />
-      </View>
+      </KeyboardAvoidingView>
     </View>
   );
 }
--- client/components/ChatInput.tsx
+++ client/components/ChatInput.tsx
@@ -1,13 +1,23 @@
-import { View, Text, TextInput } from "react-native";
+import { View, TextInput, Pressable } from "react-native";
+import Feather from "@expo/vector-icons/Feather";
 
 export default function ChatInput() {
   return (
-    <View className="flex-row justify-center items-center">
-      <View className="bg-white rounded-full px-1 mx-2 ">
-        <Text className="text-2xl">+</Text>
-      </View>
-      <View className="flex-grow bg-white rounded-full">
-        <TextInput className="text-2xl px-2" multiline />
+    <View className="flex-row justify-center items-center px-4">
+      <Pressable
+        onPressIn={() => console.log("Plus")}
+        className="bg-white rounded-full p-2 mr-2 "
+      >
+        <Feather name="plus" size={24} color="black" />
+      </Pressable>
+      <View className="flex-row flex-1 bg-white rounded-full max-h-20">
+        <TextInput className="text-2xl px-2 py-2 flex-1" multiline />
+        <Pressable
+          className="bg-green-500 px-2 m-2 rounded-full"
+          onPressIn={() => console.log("Press")}
+        >
+          <Feather name="arrow-up" size={24} color="white" />
+        </Pressable>
       </View>
     </View>
   );
